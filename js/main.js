abdd50b0a963aadb1415a506ffc1e0ed5abd9c65
Nathan Marks <NaMarks@sfusd.edu>
2019-02-12 09:35:38 -0800

    added functionality:

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 
-
 function inputSize() {
     let rowInput = $("#1st").val();
     let columnInput = $("#2nd").val();
     if ($(".matrixsize")) {
-       matrix1 = createMatrix(rowInput,columnInput);
+       let matrix1 = createMatrix(rowInput,columnInput);
     }
+    return matrix1;
 }
 function createMatrix(rows,columns){
-    matrix = [];
-    console.log("debug");
+    let matrix = [];
     for (i = 0; i <(rows); i++) {
+        matrix.push([]);
         $("#output").append("<BR/>")
         for (j=0;j<(columns);j++) {
             let matrixElement = "<input/>";
             matrixElement = $(matrixElement).attr("id", (i+1) + "," + (j+1));
             matrixElement.attr("class", 'matrix');
             $("#output").append(matrixElement);
-            matrix.push();
+            matrix[i].push("");
         }
     }
+    return matrix;
 }
+/*function updateArray(){
+    for(i=0;i<;i++){
+        for(j=0;j<;j++){
+            let testing = $("#matrix");
+
+}*/
+
 
 $(document).ready(function() {
-    $("button").click(inputSize);
+    $("#input").click(inputSize);
+   // $("#update").click(updateArray);
 });
